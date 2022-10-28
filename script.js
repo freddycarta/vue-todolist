c280402d7e3105a53785a114bc76bbdf3e24aae5
freddycarta <cartafederico92@gmail.com>
2022-10-28 17:30:54 +0200

    vue

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-const todos = [
-    {
-        text:'fare i compiti',
-        done: false,
-    },
-    {
-        text:'fare la spesa',
-        done: false,
-    },
-    {
-        text:'fare schifo',
-        done: false,
-    },
-    {
-        text:'rifarlo il giorno dopo',
-        done: false,
-    },
-    
-]
 
-const app = new Vue({
-    el: '#app',
-    data: {
-        todos: todos,
-    },
-})
+
+const {createApp} = Vue;
+
+createApp(
+    {
+        data() {
+            return{
+                activeIndex: 0,
+                todos: [
+                    {
+                        text:'fare i compiti',
+                        done: false,
+                    }, 
+                    {
+                        text:'fare le valigie',
+                        done: false,
+                    },
+                    {
+                        text:'fare la pace',
+                        done: false,
+                    },
+                    {
+                        text:'fare schifo',
+                        done: false,
+                    },
+                    ],
+            }
+        },
+        methods: {
+            
+        }
+
+    }
+).mount('#app')
+
